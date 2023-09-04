2cdfec7ddb0b40d3ddf524dbbcbbd5567b7332fd
TSShrol <shrolts@gmail.com>
2023-09-04 18:58:54 +0300

    add IRepository and Repository

---
+++ DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+
+namespace DataAccess.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> Get(
+               Expression<Func<TEntity, bool>> filter = null,
+               Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+               params string[] includeProperties);
+        TEntity GetByID(object id);
+        void Delete(object id);
+        void Delete(TEntity entityToDelete);
+        void Update(TEntity entityToUpdate);
+    }           
+}
